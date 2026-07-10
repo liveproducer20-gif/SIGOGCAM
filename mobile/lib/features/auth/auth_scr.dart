802a1d8fbe552b81a37b4cb007082311a8e44304
Jormix <liveproducer20@gmail.com>
2026-07-09 19:22:33 -0500

    logos 2.1

--- mobile/lib/features/auth/auth_scr.dart
+++ mobile/lib/features/auth/auth_scr.dart
@@ -161,8 +161,8 @@ class _AuthFrmPanelState extends State<_AuthFrmPanel> {
                   children: [
                     Image.asset(
                       'assets/img/logo_sigo_gcam.png',
-                      height: 64,
-                      width: 64,
+                      height: 100,
+                      width: 100,
                     ),
                     const SizedBox(width: 12),
                     const Expanded(
@@ -275,8 +275,8 @@ class _AuthFrmPanelState extends State<_AuthFrmPanel> {
                 Center(
                   child: Image.asset(
                     'assets/img/sigo_gcam.png',
-                    height: 68,
-                    width: 68,
+                    height: 100,
+                    width: 100,
                   ),
                 ),
                 const SizedBox(height: 8),
